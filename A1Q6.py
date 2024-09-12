81e2bbbc14cd9884417fb14c8082803aac634e14
Naomi <aomiaki2022@gmail.com>
2024-09-12 11:59:28 +1000

    create load record function

--- A1Q6.py
+++ A1Q6.py
@@ -2,7 +2,29 @@
 user_choice = input("Choose menu option: \n 1.Load records \n 2.Display \n 3.Add record \n 4.Delete record \n 5.Save records \n 6.Exit \n Select: ")
 
 # Load_records function
+import csv
+# global array
+product_ids = []
+product_names = []
+categories = []
+prices = []
+quantity_in_stock = []
+suppliers = []
+csv_file = "product_data2.csv"
 
+def load_record(file):
+    with open(file, mode='r') as product_inventory:
+        product_data = csv.reader(product_inventory)
+        # skip header row
+        next(product_data)
+        # add product detail data to matrix format
+        for row in product_data:
+            product_ids.append(int(row[0]))
+            product_names.append(row[1])
+            categories.append(row[2])
+            prices.append(float(row[3]))
+            quantity_in_stock.append(int(row[4]))
+            suppliers.append(row[5])
 
 # Display
 
