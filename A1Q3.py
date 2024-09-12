85a43bb91edac2560a7349448f84aa667a025215
Naomi <aomiaki2022@gmail.com>
2024-09-12 11:02:56 +1000

    Q3 Part 1

--- A1Q3.py
+++ A1Q3.py
@@ -1,1 +1,19 @@
-print("hello world")
+import csv
+
+# Define empty arrays for each column
+product_ids = []
+product_names = []
+categories = []
+prices = []
+quantity_in_stock = []
+suppliers = []
+
+# load data
+csv_file = "product_data.csv"
+
+# Read Data into the Program’s Parallel Arrays
+with open(csv_file, mode='r') as product_inventory:
+    product_data = csv.reader(product_inventory)
+
+    # skip header row
+    next(product_data)
