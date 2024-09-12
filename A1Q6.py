afe7a62d1104afaaba7a7bfdee5ced43130e2778
EmmaZhang <emmazxd@gmail.com>
2024-09-12 11:55:43 +1000

    Q6 add comment for each steps

--- A1Q6.py
+++ A1Q6.py
@@ -1,1 +1,36 @@
+
 user_choice = input("Choose menu option: \n 1.Load records \n 2.Display \n 3.Add record \n 4.Delete record \n 5.Save records \n 6.Exit \n Select: ")
+
+# Load_records function
+
+
+# Display
+
+
+# Add record
+
+
+# Delete record
+
+
+# Save record
+
+
+# Exit
+
+
+
+
+
+
+
+def switch_case(value):
+    match value:
+        case 1:
+            return "One"
+        case 2:
+            return "Two"
+        case 3:
+            return "Three"
+        case _:
+            return "Unknown"
