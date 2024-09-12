7f1b516d39948c947a3ba8827f1fd5291cdb6551
Naomi <aomiaki2022@gmail.com>
2024-09-12 11:04:02 +1000

    Q3 part 2

--- A1Q3.py
+++ A1Q3.py
@@ -17,3 +17,22 @@ with open(csv_file, mode='r') as product_inventory:
 
     # skip header row
     next(product_data)
+ # Read over each row in the CSV file
+    for row in product_data:
+        # Access each column with proper header stripping
+        # Strip spaces from values
+        product_ids.append(int(row[0]))
+        product_names.append(row[1])
+        categories.append(row[2])
+        prices.append(float(row[3]))
+        quantity_in_stock.append(int(row[4]))
+        suppliers.append(row[5])
+
+# Output the parallel arrays
+print("Product IDs:", product_ids)
+print("Product Names:", product_names)
+print("Categories:", categories)
+print("Prices:", prices)
+print("Quantity_in_stock:", quantity_in_stock)
+print("Suppliers:", suppliers)
+
